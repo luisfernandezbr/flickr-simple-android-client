a41e6c1935d1e92e37fa5f4bd0cb30b6ea7e11ae
Luis E. Fernandez <luisfernandezbr@gmail.com>
2016-11-05 12:59:01 -0200

    Merged Comments model classes https://github.com/luisfernandezbr/flickr-simple-android-client/issues/5

--- app/src/main/java/br/com/mobiplus/flickr/model/Comments.java
+++ app/src/main/java/br/com/mobiplus/flickr/model/Comments.java
@@ -5,6 +5,9 @@ package br.com.mobiplus.flickr.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Comments {
 
@@ -12,22 +15,54 @@ public class Comments {
     @Expose
     private String content;
 
+    @SerializedName("photo_id")
+    @Expose
+    private String photoId;
+
+    @SerializedName("comment")
+    @Expose
+    private List<Comment> comment = new ArrayList<>();
+
     /**
-     * 
-     * @return
-     *     The content
+     * @return The content
      */
     public String getContent() {
         return content;
     }
 
     /**
-     * 
-     * @param content
-     *     The _content
+     * @param content The _content
      */
     public void setContent(String content) {
         this.content = content;
+    }
+
+    /**
+     * @return The photoId
+     */
+    public String getPhotoId() {
+        return photoId;
+    }
+
+    /**
+     * @param photoId The photo_id
+     */
+    public void setPhotoId(String photoId) {
+        this.photoId = photoId;
+    }
+
+    /**
+     * @return The comment
+     */
+    public List<Comment> getComment() {
+        return comment;
+    }
+
+    /**
+     * @param comment The comment
+     */
+    public void setComment(List<Comment> comment) {
+        this.comment = comment;
     }
 
 }
