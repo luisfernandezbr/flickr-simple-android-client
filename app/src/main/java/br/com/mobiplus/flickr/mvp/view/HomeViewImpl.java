61715da7b07586c6d0ab614f52958bece05a727b
Luis E. Fernandez <luisfernandezbr@gmail.com>
2016-11-06 18:52:10 -0200

    Using simple-recyclerview in HomeView (don't showing data yet)

--- app/src/main/java/br/com/mobiplus/flickr/mvp/view/HomeViewImpl.java
+++ app/src/main/java/br/com/mobiplus/flickr/mvp/view/HomeViewImpl.java
@@ -1,10 +1,14 @@
 package br.com.mobiplus.flickr.mvp.view;
 
 import android.app.Activity;
+import android.support.annotation.IdRes;
 
 import br.com.mobiplus.flickr.R;
+import br.com.mobiplus.flickr.mvp.model.pojo.Photo2;
 import br.com.mobiplus.flickr.mvp.model.pojo.PhotoSearchResult;
 import br.com.mobiplus.mvp.view.BaseView;
+import br.com.mobiplus.simplerecylerview.SimpleLinearRecyclerView;
+import br.com.mobiplus.simplerecylerview.adapter.OnItemClickListener;
 
 /**
  * Created by luis.fernandez on 11/6/16.
@@ -29,6 +33,13 @@ public class HomeViewImpl extends BaseView implements HomeView {
 
     @Override
     public void onLoadData(PhotoSearchResult photoSearchResult) {
+        SimpleLinearRecyclerView recyclerView = (SimpleLinearRecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setCollection(photoSearchResult.getPhotos().getPhoto(), new OnItemClickListener<Photo2>() {
 
+            @Override
+            public void onItemClick(Photo2 photo, @IdRes int idViewClicked) {
+
+            }
+        });
     }
 }
